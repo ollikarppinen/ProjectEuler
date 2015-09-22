4516aad1a4938a8fee7b2eb3e37262fcb69d89d9
Olli <ollikarppinen@gmail.com>
2015-09-22 13:33:49 +0300

    Solution to problem 46 added.

--- src/P46.scala
+++ src/P46.scala
@@ -17,9 +17,24 @@ It turns out that the conjecture was false.
 What is the smallest odd composite that cannot be written as the sum of a prime and twice a square?
 
  */
-
+import scala.collection.mutable.Buffer
 
 object P46 extends App {
-  
+  val t = System.currentTimeMillis()
+  val i = Iterator.from(1).map(_ * 2 + 1) // Odd number iterator
+  val pb = Buffer(2) // Prime buffer
+  val cb = Buffer.empty[Int] //Odd composite number buffer
+  var ans = 0
+  while (ans == 0) {
+    val n = i.next
+    if (pb.forall(n % _ != 0)) { // Primality test
+      pb += n // If test is passed add n to prime buffer
+    } else {
+      cb += n // Else add n to composite buffer
+      if (pb.forall(p => math.sqrt((n - p) / 2) % 1 != 0)) ans = n // Check if conjecture holds for n
+    }
+  }
+  println("Run-time: " + (System.currentTimeMillis() - t) + "ms")
+  println("Answer: " + ans)
 }
 
