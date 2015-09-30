a5d18c0ddeb768c71537886b2263d16d4187ba41
Olli <ollikarppinen@gmail.com>
2015-10-01 01:00:40 +0300

    Solution to problem 50 added.

--- src/P49.scala
+++ src/P49.scala
@@ -1,7 +1,38 @@
 
-
 object P49 extends App {
+  val t = System.currentTimeMillis()
+  val p = PrimeTools.sieve(9999)
+  var i = 1489
+  var ans = 0l
+  while (ans == 0) {
+    if (p(i)) {
+      var j = i + 2
+      var k = j + j - i
+      while (k < 9999) {
+        if (p(j) && p(k)) {
+          if (PrimeTools.toDigits(i) == PrimeTools.toDigits(k) && 
+              PrimeTools.toDigits(i) == PrimeTools.toDigits(j))
+            ans = (i.toString + j.toString + k.toString).toLong
+        }
+        j += 2
+        k += 4
+      }
+      
+    }
+    
+    i += 2
+  }
+  println(ans)
+  println((System.currentTimeMillis() - t) + " ms.")
   
-  
-  
+  val q = PrimeTools.sieve(9999).zipWithIndex
+                                .filter(_._1)
+                                .map(_._2)
+                                .dropWhile(_ < 1000)
+                                .groupBy((PrimeTools.toDigits(_).sorted))
+                                .map(_._2)
+                                .filter(_.size >= 3)
+                                .filter(a => a.map(x => a.filter(_ < x).map(y => (y << 1) - x)).map(a.contains(_)).reduce(_ ^ _))
+//                                .map(a => a.map(x => a.filter(_ < x).map(y => (y << 1) - x)).map(a.contains(_))).map(z => z.reduce(_ ^ _))//.map(a.contains(_))))
+                                    
 }
--- src/PrimeTools.scala
+++ src/PrimeTools.scala
@@ -1,18 +1,31 @@
+import scala.collection.mutable.ArrayBuffer
 
 object PrimeTools {
   def sieve(n: Int) = {
-    val a = Array.fill(n - 1)(true)
+    val a = Array.fill(n + 1)(true)
+    a(0) = false
+    a(1) = false
     var i = 2
     while (i * i < n) {
-      if (a(i - 2)) {
+      if (a(i)) {
         var j = i + i
         while (j <= n) {
-          a(j - 2) = false
+          a(j) = false
           j = j + i
         }
       }
       i += 1
     }
-    a.zipWithIndex.filter(_._1).map(_._2 + 2)
+    a
+  }
+  
+  def toDigits(n: Int) = {
+    var i = n
+    val b = ArrayBuffer.empty[Int]
+    while (i > 0) {
+      b += i % 10
+      i = i / 10
+    }
+    b
   }
 }
