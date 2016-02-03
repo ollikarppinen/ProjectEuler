8c9473fa7c2149c834009b2c91688c0cbf6fe254
ollikarppinen <ollikarppinen@gmail.com>
2016-02-03 23:09:51 +0200

    Decoding errors fixed

--- src/P47.scala
+++ src/P47.scala
@@ -3,14 +3,14 @@ Distinct primes factors
 Problem 47
 The first two consecutive numbers to have two distinct prime factors are:
 
-14 = 2 � 7
-15 = 3 � 5
+14 = 2 * 7
+15 = 3 * 5
 
 The first three consecutive numbers to have three distinct prime factors are:
 
-644 = 2� � 7 � 23
-645 = 3 � 5 � 43
-646 = 2 � 17 � 19.
+644 = 2 * 7 * 23
+645 = 3 * 5 * 43
+646 = 2 * 17 * 19.
 
 Find the first four consecutive integers to have four distinct prime factors. What is the first of these numbers?
  */
@@ -56,5 +56,5 @@ object P47 extends App {
   }
   println("Run-time: " + (System.currentTimeMillis() - t) + "ms")
   println(ans)
-  
+
 }
--- src/P49.scala
+++ src/P49.scala
@@ -10,29 +10,29 @@ object P49 extends App {
       var k = j + j - i
       while (k < 9999) {
         if (p(j) && p(k)) {
-          if (PrimeTools.toDigits(i) == PrimeTools.toDigits(k) && 
-              PrimeTools.toDigits(i) == PrimeTools.toDigits(j))
+          if (PrimeTools.toDigits(i) == PrimeTools.toDigits(k) &&
+            PrimeTools.toDigits(i) == PrimeTools.toDigits(j))
             ans = (i.toString + j.toString + k.toString).toLong
         }
         j += 2
         k += 4
       }
-      
+
     }
-    
+
     i += 2
   }
   println(ans)
   println((System.currentTimeMillis() - t) + " ms.")
-  
+
   val q = PrimeTools.sieve(9999).zipWithIndex
-                                .filter(_._1)
-                                .map(_._2)
-                                .dropWhile(_ < 1000)
-                                .groupBy((PrimeTools.toDigits(_).sorted))
-                                .map(_._2)
-                                .filter(_.size >= 3)
-                                .filter(a => a.map(x => a.filter(_ < x).map(y => (y << 1) - x)).map(a.contains(_)).reduce(_ ^ _))
-//                                .map(a => a.map(x => a.filter(_ < x).map(y => (y << 1) - x)).map(a.contains(_))).map(z => z.reduce(_ ^ _))//.map(a.contains(_))))
-                                    
+    .filter(_._1)
+    .map(_._2)
+    .dropWhile(_ < 1000)
+    .groupBy((PrimeTools.toDigits(_).sorted))
+    .map(_._2)
+    .filter(_.size >= 3)
+    .filter(a => a.map(x => a.filter(_ < x).map(y => (y << 1) - x)).map(a.contains(_)).reduce(_ ^ _))
+  //                                .map(a => a.map(x => a.filter(_ < x).map(y => (y << 1) - x)).map(a.contains(_))).map(z => z.reduce(_ ^ _))//.map(a.contains(_))))
+
 }
